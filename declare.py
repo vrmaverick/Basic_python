c8d99cfaaa96fc185740d2094a10b7a88cdc0252
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-01-24 21:25:51 +0530

    tupples

---
+++ declare.py
@@ -0,0 +1,10 @@
+t1=(1,2,3,4,5)
+print(t1)
+# t1[0]=56 cannot modify tupple
+# print(t1)
+t2=()#empty
+print(t2)
+t3=("vedant",)#only single element
+print(t3)
+t4=("vedant","ranade",26,12)
+print(t4)
---
+++ tupple methods.py
@@ -0,0 +1,4 @@
+t3=("vedant","ranade",26,12,"vedant")
+print(t3.count("vedant"))#2
+print(t3.index("vedant"))#position
+
