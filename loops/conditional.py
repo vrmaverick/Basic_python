5b7674c015f52c6807075d39b550bfd3dc921dec
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-01-28 15:58:02 +0530

    Add files via upload

---
+++ loops/conditional.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age : "))
+if(age<18):
+    print("Cannot drive you are underage")
+elif(age>55):
+    print("Can drive after checking vision")
+else:
+    print("Allowed to drive")
