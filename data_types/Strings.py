e2a1ef1a2ddae5d7f4cf10e6fd292bb9bd76a800
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-24 14:47:53 +0530

    Update Strings.py

--- data_types/Strings.py
+++ data_types/Strings.py
@@ -10,3 +10,17 @@ _____
 '''
 merge = Username+password #no space
 print(merge)
+
+a = 100
+b = str(a)
+c = int (b)
+print(type(b))
+print(type(c))
+
+name = input("enter your name ")
+age = int (input("enter your age "))
+city = input("enter your city ")
+
+print('HIII'+ name+ ' your age is '+str(age)+'\n\tIt\'s sunny outside in '+ city)#normal
+print(f'HIII {name} your age is {age}\n\tIt\'s sunny outside in {city}')#formatted
+print('hii {new_name} your age is {0} it is to cold outside in {1}'.format(age,city,new_name='bot'))
