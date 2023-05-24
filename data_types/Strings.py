f8ec68b5d480b35c679c9d652e17d5c759e12558
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-24 14:58:33 +0530

    STRING manipulation

--- data_types/Strings.py
+++ data_types/Strings.py
@@ -24,3 +24,17 @@ city = input("enter your city ")
 print('HIII'+ name+ ' your age is '+str(age)+'\n\tIt\'s sunny outside in '+ city)#normal
 print(f'HIII {name} your age is {age}\n\tIt\'s sunny outside in {city}')#formatted
 print('hii {new_name} your age is {0} it is to cold outside in {1}'.format(age,city,new_name='bot'))
+
+# STRING MANIPULATION
+s = "0123456789"
+#+ve index 0123.....
+#-ve index -9-8-7........
+# [start:end:step],default 0
+#end -1 tak
+print(s[3])
+print(s[3:])
+print(s[3:8])
+print(s[3:9:2])
+print(s[::2])
+print(s[::-1])
+print(s[-2:-9:-2])
