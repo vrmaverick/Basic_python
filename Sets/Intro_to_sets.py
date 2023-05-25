47aded2680fdac9d0af4ff462202b771fbbc18fa
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-25 22:52:37 +0530

    Update Intro_to_sets.py

--- Sets/Intro_to_sets.py
+++ Sets/Intro_to_sets.py
@@ -9,3 +9,14 @@ print("......................................................................")
 b = set()#set can also be declared this way
 print(type(b))
 print("......................................................................")
+
+my_list=[1,2,3,4,5,5,5,6,6,7,4,3,6,4,]
+my_set=set(my_list)
+print(my_set) #unique values
+print(6 in my_set)
+print(len(my_set))
+print(len(my_list))
+n = my_set.copy()
+my_set.clear()
+print(n)
+print(my_set)
