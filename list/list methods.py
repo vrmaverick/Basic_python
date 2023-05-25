c13567d24e492aeeaaf0e807f6fda0cc8b31963c
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-25 13:20:15 +0530

    Update list methods.py

--- list/list methods.py
+++ list/list methods.py
@@ -1,22 +1,45 @@
-a = [26,12,2003,56,79,69,88]#it must be same data type
+a = [26,12,2003,56,79,69,88,100]#it must be same data type
 print("unsorted : ")
 print(a)#does not concatinate list
 a.sort()#modifies a does not return
 print("sorted :")
 print(a)
+#.........................................
+# new = sorted(a)........creates a copy of a and then sorts copy
+# print(new)
+# print(a) does not affect in sorted () function
+
 a.reverse()
-print("reverse : ")
+print("reverse : ") #decending order
 print(a)
+
+
+#adding
+
 a.append(100)#adds 100 at end of list
 print("100 is appended at end : ")
 print(a)
 a.insert(0,544)#insert 544 at 0th index.....l1.insert(index,value)
 print("at 0th index 545 is added in list : ")
 print(a)
-a.pop(0)
-print("0th index poped : ")
+a.insert(100,689)#last position as there are no 100 elements
 print(a)
-a.remove(100)
+a.extend([100,200,300,400,500])
+print(a)
+
+c=a.index(200)
+print(c) #returns index of which the first itself occurs
+print(a.index(200,2,100))#start from index 2 to index 99 but there is no index 99 so till end of list.....gives error if not found
+print(200 in a[11:])
+
+c=a.count(100)
+print (c)
+
+#removing
+b= a.pop(0) #returns popped value
+print("0th index poped : ",b)
+print(a) #removes that value from list
+a.remove(100) #returns nothing 
 print("100 is removed : ")
 print(a)
 
