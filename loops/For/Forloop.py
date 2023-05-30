6a7bd55662ef4d671f2dc34963967806e941bffe
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-30 15:17:08 +0530

    Update Forloop.py

--- loops/For/Forloop.py
+++ loops/For/Forloop.py
@@ -6,3 +6,18 @@ sum = 0
 for i in range(1,100,2):#range (start,end,step-size)
     sum = sum + i
 print("sum is:",sum)#sum of 100 odd numbers
+
+#in case of dictionaries we use few functions to iterate
+#for loops cannot be used with integers as they are not iterable
+user = {
+    'name':'vedant',
+    'age':19,
+    'passed': True
+}
+for i in user.keys():
+    print(i)
+print('&'*80)
+for i in user.items():
+    key,value = i
+    print(i)
+    print(key,value)
