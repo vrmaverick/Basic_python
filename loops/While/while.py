c8d874c02b294eff96750c065717b3d5a69ba9ef
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-30 22:48:47 +0530

    Update while.py

--- loops/While/while.py
+++ loops/While/while.py
@@ -3,3 +3,9 @@ while i<10 :
     print("yes",str(i))
     i = i + 1
 print("done now i =",i)
+
+#logic used in bots when user types bye the program terminates
+while True:
+    i=input("say something : ")
+    if(i=='bye'):
+        break
