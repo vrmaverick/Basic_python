1898751ab92638e994e922c117fa923959e3e61b
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-30 14:47:59 +0530

    Create if_else.py

---
+++ loops/if_else.py
@@ -0,0 +1,8 @@
+magician = True
+expert= False
+if magician and expert:
+    print("you are expert magician")
+elif magician and not(expert):
+    print("you will have to practice")
+else:
+    print("You need to learn magic")
