d44d8cd84a78c3f2764f98f5febe34d720948a18
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-05-31 16:25:59 +0530

    Update nested.py

--- Functions/nested.py
+++ Functions/nested.py
@@ -1,6 +1,10 @@
 def sum(num1,num2):
+    '''
+     Info: This function returns another function which returns the sum
+    '''
     def another_function(n1,n2):
         return n1 + n2
     return another_function(num1,num2)
 print(sum(10,20))
+print(sum.__doc__)
 #cannot call another function directly from main
