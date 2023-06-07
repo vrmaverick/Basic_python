f59053217ce72d1815157a2601e9d9a2020d16dc
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-06-07 23:42:51 +0530

    Update lamda_funct.py

--- Functional_programming/lamda_funct.py
+++ Functional_programming/lamda_funct.py
@@ -1,6 +1,10 @@
 from functools import reduce
-my_list = [1, 2, 3]
-print(list(map(lambda item: item * 2, my_list)))
+my_list = [2, 3, 5]
+print(list(map(lambda item: item ** 2, my_list)))
 print(list(filter(lambda item: item % 2 != 0, my_list)))
 print(reduce(lambda acc, item: item*acc, my_list, 10))
+# lamda used for single use functions for memory efficiency but decrease readability
+print("sum of squares of list items is : ")
+print(reduce(lambda acc, item: acc+item, (map(lambda num: num**2, my_list)), 0))
+
  #lamda used for single use functions for memory efficiency but decrease readability
