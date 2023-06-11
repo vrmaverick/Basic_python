fa54286156804385274c1e167da4c37a0b81615e
vrmaverick <119831329+vrmaverick@users.noreply.github.com>
2023-06-11 23:10:02 +0530

    Update Error_handling.py

--- Error_handling.py
+++ Error_handling.py
@@ -4,11 +4,14 @@ while True:
         i = 10/year
     except ValueError:  # only for value error
         print('Enter a number not a string')
-    except ZeroDivisionError:
+ except ZeroDivisionError as err:
         print('number cannot be 0')
+        print(err) #display error
     except:
         print("for all other errors")
         break
     else:
         print("no errors detected while loop terminates here!!!!!!!!", i)
         break
+    finally:
+        print('this will print whatever happens')
